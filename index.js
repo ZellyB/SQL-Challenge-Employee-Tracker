71567deaa2fb54027106368a2f0d763744f31fff
kelly boothe <kellygageboothe@gmail.com>
2023-04-12 04:35:51 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const promptMenu = [
+    {
+      message: `What would you like to do?`,
+      type: `list`,
+      choices: [
+        `View All Departments`,
+        `View All Roles`,
+        `View All Employees`,
+        `Add Department`,
+        `Add Role`,
+        `Add Employee`,
+        `Update Employee Role`,
+        `Exit`,
+      ],
+      name: 'action'
+    },
+  ];
